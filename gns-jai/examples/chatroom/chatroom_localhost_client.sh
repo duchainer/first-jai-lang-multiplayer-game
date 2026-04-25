9f1976d80239464776edb57ef35ac9905df971ca
raph <duchainer@protonmail.com>
2026-04-24 21:36:56 -0400

    Add linux run script for gns-jai example

---
+++ gns-jai/examples/chatroom/chatroom_localhost_client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+echo "chatroom program (client) connecting to local host"
+./first -client loopback
---
+++ gns-jai/examples/chatroom/chatroom_localhost_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+echo "chatroom program (server) as local host"
+./first -server
